c7a7ecd64066566b280d120e4726a230479239e8
Terrence Eve <tijayeve@gmail.com>
2019-11-12 19:00:44 -0600

    accessed App component

--- nameTags/App.js
+++ nameTags/App.js
@@ -1,7 +1,10 @@
 // Render the name tags using React!
 const App = (props) => {
     let { nameTags } = props;
-    console.log("App component props:", props)
+    console.log("Line 4 -->> App component props:", props)
+
+    const AllNametags = props.nameTags.map(NameTag) //maps over NameTag function on line 15
+
     return (
         <span>name tags</span>
     )
@@ -10,9 +13,15 @@ const App = (props) => {
 // CHALLENGE: Write a separate NameTag component for use in the App component
 // TIP: Use props to pass the name to each NameTag component
 const NameTag = (props) => {
-
     console.log("NameTag component props:", props)
 
+    const name = {
+        name: props.name
+    }
+
+    return (
+        <div>{name}</div>
+    )
     
 
 
